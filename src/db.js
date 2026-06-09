461bf2c03e4fae9c3567703a4e43d2a50d54d151
Tivytin <victorsouzadev9@gmail.com>
2026-06-09 18:00:35 -0300

    Conecta PostgreSQL Railway

--- src/db.js
+++ src/db.js
@@ -1,13 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",       // seu usuário do pgAdmin
-    host: "localhost",
-    database: "agendamento",      // nome do banco que você criou
-    password: "123456",       // sua senha do postgres
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
-    query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params)
 };
