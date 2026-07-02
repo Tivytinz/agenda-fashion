723a94438fc0c1e34b2e59b3e5b9c0ffe8b061e1
Tivytin <victorsouzadev9@gmail.com>
2026-07-01 22:35:35 -0300

    fix: corrigir prefixo das rotas de conta

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ router.use("/", require("./authRoutes"));
 router.use("/", require("./negocioRoutes"));
 router.use("/", require("./checkoutRoutes"));
 router.use("/", require("./assinaturaRoutes"));
-router.use("/", require("./contaRoutes"));
+router.use("/conta", require("./contaRoutes"));
 router.use("/", require("./configuracoesRoutes"));
 router.use("/", require("./profissionaisRoutes"));
 router.use("/", require("./servicosRoutes"));
@@ -19,14 +19,5 @@ router.use("/", require("./agendaRoutes"));
 router.use("/", require("./perfilNegocioRoutes"));
 router.use("/", require("./agendaPublicaRoutes"));
 router.use("/", require("./adminRoutes"));
-
-
-// Os próximos módulos serão adicionados aqui conforme forem sendo separados:
-// router.use("/", require("./agendaRoutes"));
-// router.use("/", require("./agendaPublicaRoutes"));
-// router.use("/", require("./dashboardRoutes"));
-// router.use("/", require("./favoritosRoutes"));
-// router.use("/", require("./configuracoesRoutes"));
-// router.use("/", require("./adminRoutes"));
 
 module.exports = router;
