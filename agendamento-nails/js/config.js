86d08ed47fd0908787e9cdbc7f8bb18d137f0ae7
Tivitinz <victorsouzadev9@gmail.com>
2026-07-30 19:17:13 -0300

    refactor: remove frontend legado e consolida React

--- agendamento-nails/js/config.js
+++
@@ -1,9 +0,0 @@
-window.AF_CONFIG = {
-  API_URL:
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-      ? "http://localhost:3000"
-      : "https://app.agendafashion.com.br",
-};
-
-window.API_URL = window.AF_CONFIG.API_URL;
