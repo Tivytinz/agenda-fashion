3d03db65d610fb1a4e5364d29fcdc58a1ef6d17c
Tivytinz <victorsouzadev9@gmail.com>
2026-08-11 01:40:10 -0300

    Adiciona controller de medição Google

---
+++ src/controllers/googleMeasurementController.js
@@ -0,0 +1,69 @@
+const googleMeasurementService = require(
+  "../services/googleMeasurementService"
+);
+const AppError = require(
+  "../errors/AppError"
+);
+
+function configuracaoPublica(
+  _req,
+  res,
+  next
+) {
+  try {
+    return res.status(200).json(
+      googleMeasurementService
+        .obterConfiguracaoPublica()
+    );
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
+async function atualizarConsentimento(
+  req,
+  res,
+  next
+) {
+  try {
+    if (
+      typeof req.body?.consentimento !==
+      "boolean"
+    ) {
+      throw new AppError(
+        "Informe uma escolha válida de privacidade.",
+        400
+      );
+    }
+
+    const contexto =
+      googleMeasurementService
+        .sanitizarContextoCliente(
+          req.body || {}
+        );
+
+    await googleMeasurementService
+      .salvarConsentimento({
+        usuarioId: req.user?.id,
+        google: {
+          consentimento:
+            contexto.consentimento,
+          client_id:
+            contexto.clientId
+        }
+      });
+
+    return res.status(200).json({
+      salvo: true,
+      consentimento:
+        contexto.consentimento
+    });
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
+module.exports = {
+  configuracaoPublica,
+  atualizarConsentimento
+};
