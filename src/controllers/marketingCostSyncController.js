be7162f1101f7e49cd625fa0203d3df907c55a79
Tivytinz <victorsouzadev9@gmail.com>
2026-08-12 16:37:25 -0300

    expose external campaign discovery endpoint

--- src/controllers/marketingCostSyncController.js
+++ src/controllers/marketingCostSyncController.js
@@ -8,6 +8,16 @@ async function status(req, res, next) {
   }
 }
 
+async function listarCampanhas(req, res, next) {
+  try {
+    return res.status(200).json(await service.listarCampanhasExternas({
+      provedor: req.params?.provedor
+    }));
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
 async function vincular(req, res, next) {
   try {
     return res.status(200).json(await service.vincularCampanha({ payload: req.body }));
@@ -28,4 +38,4 @@ async function sincronizar(req, res, next) {
   }
 }
 
-module.exports = { status, vincular, sincronizar };
+module.exports = { status, listarCampanhas, vincular, sincronizar };
