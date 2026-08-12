8b5791227f93b40357ebc2f8be5e0db71095fee4
Tivytinz <victorsouzadev9@gmail.com>
2026-08-12 18:25:47 -0300

    Fase 12: libera Pinterest e TikTok nos custos

---
+++ database/migrations/038_marketing_pinterest_tiktok.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+ALTER TABLE marketing_campanha_vinculos
+  DROP CONSTRAINT marketing_campanha_vinculos_provedor_valido;
+
+ALTER TABLE marketing_campanha_vinculos
+  ADD CONSTRAINT marketing_campanha_vinculos_provedor_valido
+  CHECK (
+    provedor IN (
+      'google_ads',
+      'meta_ads',
+      'pinterest_ads',
+      'tiktok_ads'
+    )
+  );
+
+ALTER TABLE marketing_custo_sincronizacoes
+  DROP CONSTRAINT marketing_custo_sincronizacoes_provedor_valido;
+
+ALTER TABLE marketing_custo_sincronizacoes
+  ADD CONSTRAINT marketing_custo_sincronizacoes_provedor_valido
+  CHECK (
+    provedor IN (
+      'google_ads',
+      'meta_ads',
+      'pinterest_ads',
+      'tiktok_ads'
+    )
+  );
+
+COMMENT ON COLUMN marketing_campanha_gastos.fonte IS
+  'Origem efetiva do custo do dia: manual, google_ads, meta_ads, pinterest_ads ou tiktok_ads. A fonte gravada por último substitui as demais para impedir dupla contagem.';
+
+COMMIT;
