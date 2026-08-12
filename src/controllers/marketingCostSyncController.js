5789c5d7d5335541a698e3a45aa0e71228a2f738
Tivytinz <victorsouzadev9@gmail.com>
2026-08-12 16:53:26 -0300

    add marketing integration connection test route handler

--- src/controllers/marketingCostSyncController.js
+++ src/controllers/marketingCostSyncController.js
@@ -18,6 +18,16 @@ async function listarCampanhas(req, res, next) {
   }
 }
 
+async function testar(req, res, next) {
+  try {
+    return res.status(200).json(await service.testarIntegracao({
+      provedor: req.params?.provedor
+    }));
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
 async function vincular(req, res, next) {
   try {
     return res.status(200).json(await service.vincularCampanha({ payload: req.body }));
@@ -38,4 +48,4 @@ async function sincronizar(req, res, next) {
   }
 }
 
-module.exports = { status, listarCampanhas, vincular, sincronizar };
+module.exports = { status, listarCampanhas, testar, vincular, sincronizar };
