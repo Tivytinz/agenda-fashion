d20794e8cc5729df6bb48e9da3a9243ccafe7ef5
Tivytinz <victorsouzadev9@gmail.com>
2026-08-23 16:50:39 -0300

    style: carrega refinamentos do perfil público

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import "./styles/admin-saas-health.css";
 import "./styles/admin-whatsapp.css";
 import "./styles/af-experience.css";
 import "./styles/home-discovery.css";
+import "./styles/profile-polish.css";
 
 installRuntimeRecovery();
 
