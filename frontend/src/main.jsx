3d06246febbb53b5c7828f51bf79d98d1452b67e
Tivytinz <victorsouzadev9@gmail.com>
2026-08-23 21:54:17 -0300

    style: load plan state polish

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,6 +22,7 @@ import "./styles/service-catalog-polish.css";
 import "./styles/schedule-polish.css";
 import "./styles/business-polish.css";
 import "./styles/subscription-polish.css";
+import "./styles/plans-polish.css";
 
 installRuntimeRecovery();
 
