c3454b99599bab6d37ea8003d3f194b5ec277b15
Tivytinz <victorsouzadev9@gmail.com>
2026-09-15 19:10:51 -0300

    style: carrega estilos do ciclo de atendimento

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import "./styles/af-experience.css";
 import "./styles/home-discovery.css";
 import "./styles/profile-polish.css";
 import "./styles/account-polish.css";
+import "./styles/lifecycle.css";
 
 installRuntimeRecovery();
 
