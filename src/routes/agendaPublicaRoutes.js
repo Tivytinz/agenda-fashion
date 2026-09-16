cd6d6f48e5d28c16b4dc157dc379bc7506938114
Tivytinz <victorsouzadev9@gmail.com>
2026-09-15 22:55:41 -0300

    feat: conecta acesso e cancelamento de visitante

--- src/routes/agendaPublicaRoutes.js
+++ src/routes/agendaPublicaRoutes.js
@@ -8,9 +8,50 @@ const {
 const optionalAuth = require("../middlewares/optionalAuth");
 const auth = require("../middlewares/auth");
 const agendaPublicaController = require("../controllers/agendamentoPublicoController");
+const agendamentoVisitanteController = require(
+  "../controllers/agendamentoVisitanteController"
+);
 const agendamentoLifecycleController = require(
   "../controllers/agendamentoLifecycleController"
 );
+const {
+  gerarAcessoVisitante,
+} = require("../utils/agendamentoVisitante");
+
+function anexarAcessoVisitante(
+  req,
+  res,
+  next
+) {
+  if (req.user?.id) {
+    return next();
+  }
+
+  const responderJson =
+    res.json.bind(res);
+
+  res.json = (body) => {
+    if (
+      res.statusCode === 201 &&
+      body?.agendamento?.id
+    ) {
+      return responderJson({
+        ...body,
+        agendamento: {
+          ...body.agendamento,
+          acesso_visitante:
+            gerarAcessoVisitante(
+              body.agendamento.id
+            ),
+        },
+      });
+    }
+
+    return responderJson(body);
+  };
+
+  return next();
+}
 
 /**
  * @swagger
@@ -95,7 +136,7 @@ router.get(
  *                 example: 62999999999
  *     responses:
  *       201:
- *         description: Agendamento criado
+ *         description: Agendamento criado. Visitantes recebem acesso_visitante dentro do agendamento.
  *       400:
  *         description: Dados inválidos
  *       409:
@@ -106,6 +147,7 @@ router.post(
   "/agendamentos",
   limitarAgendamento,
   optionalAuth,
+  anexarAcessoVisitante,
   agendaPublicaController.criarAgendamentoPublico
 );
 
@@ -134,7 +176,7 @@ router.get(
  * @swagger
  * /agendamentos/{id}/cancelar:
  *   patch:
- *     summary: Cancela um agendamento
+ *     summary: Cancela um agendamento da conta autenticada
  *     tags: [Agenda Pública]
  *     security:
  *       - bearerAuth: []
@@ -155,6 +197,43 @@ router.patch(
   "/agendamentos/:id/cancelar",
   auth,
   agendaPublicaController.cancelarMeuAgendamento
+);
+
+/**
+ * @swagger
+ * /agendamentos/{id}/cancelar-visitante:
+ *   patch:
+ *     summary: Cancela um agendamento visitante com a credencial emitida na criação
+ *     tags: [Agenda Pública]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [acesso_visitante]
+ *             properties:
+ *               acesso_visitante:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Agendamento cancelado
+ *       403:
+ *         description: Credencial inválida
+ *       404:
+ *         description: Agendamento visitante não encontrado
+ */
+
+router.patch(
+  "/agendamentos/:id/cancelar-visitante",
+  limitarAgendamento,
+  agendamentoVisitanteController.cancelar
 );
 
 /**
