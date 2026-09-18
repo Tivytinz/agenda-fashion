f7c9eb692328a0936164db06d3fc350cbb315c40
Tivytinz <victorsouzadev9@gmail.com>
2026-09-18 05:24:56 -0300

    feat(api): adiciona controller de serviços por profissional

---
+++ src/controllers/servicoProfissionalController.js
@@ -0,0 +1,48 @@
+const servicoProfissionalService = require(
+  "../services/servicoProfissionalService"
+);
+
+async function listarServicos(req, res, next) {
+  try {
+    const resultado =
+      await servicoProfissionalService
+        .listarServicosProfissional({
+          usuarioId:
+            req.user?.id,
+          profissionalId:
+            req.params.id,
+        });
+
+    return res.json(resultado);
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
+async function configurarServicos(
+  req,
+  res,
+  next
+) {
+  try {
+    const resultado =
+      await servicoProfissionalService
+        .configurarServicosProfissional({
+          usuarioId:
+            req.user?.id,
+          profissionalId:
+            req.params.id,
+          servicoIds:
+            req.body?.servico_ids,
+        });
+
+    return res.json(resultado);
+  } catch (erro) {
+    return next(erro);
+  }
+}
+
+module.exports = {
+  listarServicos,
+  configurarServicos,
+};
