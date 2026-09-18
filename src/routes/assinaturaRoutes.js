fdcdf8ba592e942b7b3b28575369753341dae894
Tivytinz <victorsouzadev9@gmail.com>
2026-09-18 00:35:17 -0300

    fix(assinatura): limita tentativas de reativação

--- src/routes/assinaturaRoutes.js
+++ src/routes/assinaturaRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middlewares/auth");
+const {
+  limitarCheckout
+} = require("../middlewares/rateLimits");
 const assinaturaController = require("../controllers/assinaturaController");
 
 router.get(
@@ -19,6 +22,7 @@ router.delete(
 router.post(
   "/minha-assinatura/reativar",
   auth,
+  limitarCheckout,
   assinaturaController.reativarMinhaAssinatura
 );
 
