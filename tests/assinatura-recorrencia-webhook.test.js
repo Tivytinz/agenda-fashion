b8ae368cd2abd52cc358e7823c6ed39760d5b6fc
Tivytinz <victorsouzadev9@gmail.com>
2026-09-22 13:55:08 -0300

    test(planos): rastreia critérios P0 financeiros

--- tests/assinatura-recorrencia-webhook.test.js
+++ tests/assinatura-recorrencia-webhook.test.js
@@ -851,7 +851,7 @@ describe(
     );
 
     test(
-      "suspende assinatura vencida e retorna o negócio ao plano gratuito",
+      "CA-PLN-04: falha de cobrança suspende assinatura e permite recuperação posterior",
       async () => {
         mockClient.query
           .mockImplementation(
